2cf58695d8c6fc3023c0f9406d07a57e5730497e
Harm Hanemaaijer <fgenfb@yahoo.com>
2015-04-01 01:20:00 +0200

    Extend format conversion (including conversion from RG16 to RGBX8).
    Signed-off-by: Harm Hanemaaijer <fgenfb@yahoo.com>

--- convert.c
+++ convert.c
@@ -18,6 +18,50 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 #include "detex.h"
 
+static void ConvertPixel32SwapRAndB(uint8_t *pixel_buffer, int nu_pixels) {
+	for (int i = 0; i < nu_pixels; i++) {
+		/* Swap R and B. */
+		uint8_t c0 = *pixel_buffer;
+		uint8_t c2 = *(pixel_buffer + 2);
+		*pixel_buffer = c2;
+		*(pixel_buffer + 2) = c0;
+		pixel_buffer += 4;
+	}
+}
+
+static void ConvertPixel64SwapRAndB(uint8_t *pixel_buffer, int nu_pixels) {
+	for (int i = 0; i < nu_pixels; i++) {
+		/* Swap R and B (16-bit). */
+		uint16_t c0 = *(uint16_t *)pixel_buffer;
+		uint16_t c2 = *(uint16_t *)(pixel_buffer + 4);
+		*(uint16_t *)pixel_buffer = c2;
+		*(uint16_t *)(pixel_buffer + 4) = c0;
+		pixel_buffer += 8;
+	}
+}
+
+static void ConvertPixel32RG16ToRGBX8(uint8_t *pixel_buffer, int nu_pixels) {
+	uint32_t *pixel32_buffer = (uint32_t *)pixel_buffer;
+	for (int i = 0; i < nu_pixels; i++) {
+		uint32_t pixel = *pixel32_buffer;
+		uint32_t red = detexPixel32GetR16(pixel);
+		uint32_t green = detexPixel32GetG16(pixel);
+		*pixel32_buffer = detexPack32RGB8Alpha0xFF(red >> 8, green >> 8, 0);
+		pixel32_buffer++;
+	}
+}
+
+static void ConvertPixel32RG16ToBGRX8(uint8_t *pixel_buffer, int nu_pixels) {
+	uint32_t *pixel32_buffer = (uint32_t *)pixel_buffer;
+	for (int i = 0; i < nu_pixels; i++) {
+		uint32_t pixel = *pixel32_buffer;
+		uint32_t red = detexPixel32GetR16(pixel);
+		uint32_t green = detexPixel32GetG16(pixel);
+		*pixel32_buffer = detexPack32RGB8Alpha0xFF(0, green >> 8, red >> 8);
+		pixel32_buffer++;
+	}
+}
+
 /* Convert pixels between different formats in place. Return true if succesful. */
 
 bool detexConvertPixels(uint8_t *pixel_buffer, uint32_t nu_pixels,
@@ -32,8 +76,19 @@ uint32_t source_pixel_format, uint32_t target_pixel_format) {
 		source_pixel_format);
 	int target_nu_components = detexGetNumberOfComponents(
 		target_pixel_format);
-	if (source_nu_components != target_nu_components)
+	if (source_nu_components != target_nu_components) {
+		if (source_pixel_format == DETEX_PIXEL_FORMAT_RG16) {
+			if (target_pixel_format == DETEX_PIXEL_FORMAT_RGBX8) {
+				ConvertPixel32RG16ToRGBX8(pixel_buffer, nu_pixels);
+				return true;
+			}
+			else if (target_pixel_format == DETEX_PIXEL_FORMAT_BGRX8) {
+				ConvertPixel32RG16ToBGRX8(pixel_buffer, nu_pixels);
+				return true;
+			}
+		}
 		return false;
+	}
 	if (source_pixel_size == 4) {
 		if (source_nu_components < 3)
 			return false;
@@ -42,16 +97,8 @@ uint32_t source_pixel_format, uint32_t target_pixel_format) {
 		if ((source_pixel_format &
 		DETEX_PIXEL_FORMAT_BGR_COMPONENT_ORDER_BIT) !=
 		(target_pixel_format &
-		DETEX_PIXEL_FORMAT_BGR_COMPONENT_ORDER_BIT)) {
-			for (int i = 0; i < nu_pixels; i++) {
-				/* Swap R and B. */
-				uint8_t c0 = *pixel_buffer;
-				uint8_t c2 = *(pixel_buffer + 2);
-				*pixel_buffer = c2;
-				*(pixel_buffer + 2) = c0;
-				pixel_buffer += 4;
-			}
-		}
+		DETEX_PIXEL_FORMAT_BGR_COMPONENT_ORDER_BIT))
+			ConvertPixel32SwapRAndB(pixel_buffer, nu_pixels);
 		return true;
 	}
 	else if (source_pixel_size == 8) {
@@ -69,16 +116,8 @@ uint32_t source_pixel_format, uint32_t target_pixel_format) {
 		if ((source_pixel_format &
 		DETEX_PIXEL_FORMAT_BGR_COMPONENT_ORDER_BIT) !=
 		(target_pixel_format &
-		DETEX_PIXEL_FORMAT_BGR_COMPONENT_ORDER_BIT)) {
-			for (int i = 0; i < nu_pixels; i++) {
-				/* Swap R and B. */
-				uint16_t c0 = *(uint16_t *)pixel_buffer;
-				uint8_t c2 = *(uint16_t *)(pixel_buffer + 4);
-				*(uint16_t *)pixel_buffer = c2;
-				*(uint16_t *)(pixel_buffer + 4) = c0;
-				pixel_buffer += 8;
-			}
-		}
+		DETEX_PIXEL_FORMAT_BGR_COMPONENT_ORDER_BIT))
+			ConvertPixel64SwapRAndB(pixel_buffer, nu_pixels);
 		return true;
 	}
 	else
