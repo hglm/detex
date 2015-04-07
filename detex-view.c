2972ba563ccdd10d5bb0077b91ad5aa742d4f459
Harm Hanemaaijer <fgenfb@yahoo.com>
2015-04-07 16:49:20 +0200

    Improve window size management in detex-view
    Signed-off-by: Harm Hanemaaijer <fgenfb@yahoo.com>

--- detex-view.c
+++ detex-view.c
@@ -27,15 +27,17 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 #include "detex.h"
 
-#define DEFAULT_TEXTURE_AREA_WIDTH 512
-#define DEFAULT_TEXTURE_AREA_HEIGHT 512
+#define DEFAULT_WINDOW_WIDTH 512
+#define DEFAULT_WINDOW_HEIGHT 540
 
 static GtkWidget *gtk_window;
-cairo_surface_t *area_surface;
-int area_width, area_height;
-GtkWidget *texture_label;
-uint8_t *pixel_buffer;
-detexTexture *texture;
+static cairo_surface_t *area_surface = NULL;
+static int area_width, area_height;
+static GtkWidget *texture_label;
+static detexTexture *texture = NULL;
+static uint8_t *pixel_buffer;	// Decompressed pixel buffer.
+
+static void DrawTexture(detexTexture *texture, uint8_t *pixel_buffer);
 
 static gboolean delete_event_cb(GtkWidget *widget, GdkEvent *event, gpointer data) {
 	return FALSE;
@@ -46,7 +48,21 @@ static void destroy_cb(GtkWidget *widget, gpointer data) {
 	exit(0);
 }
 
-static gboolean area_draw_cb(GtkWidget *widget, cairo_t *cr, cairo_surface_t *surface) {
+static gboolean area_configure_event_cb(GtkWidget *widget, GdkEventConfigure *event,
+GtkWidget *image_drawing_area) {
+	if (area_surface != NULL)
+		cairo_surface_destroy(area_surface);
+	area_width = event->width;
+	area_height = event->height;
+	area_surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32,
+		area_width, area_height);
+    	DrawTexture(texture, pixel_buffer);
+	gtk_widget_show_all(gtk_window);
+	// We've handled the configure event, no need for further processing.
+	return TRUE;
+}
+
+static gboolean area_draw_cb(GtkWidget *widget, cairo_t *cr, void *user_data) {
 	cairo_set_source_surface(cr, area_surface, 0, 0);
 	cairo_paint(cr);
 	return TRUE;
@@ -56,38 +72,42 @@ static void CreateWindowLayout() {
 	gtk_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(gtk_window),
 		"Detex Library Texture Viewer");
+	// Figure out the screen dimensions.
+	GdkScreen *screen = gtk_window_get_screen(GTK_WINDOW(gtk_window));
+	gint monitor = gdk_screen_get_monitor_at_window(screen, gdk_screen_get_active_window(screen));
+	GdkRectangle monitor_rect;
+	gdk_screen_get_monitor_geometry(screen, monitor, &monitor_rect);
+	int width = DEFAULT_WINDOW_WIDTH;
+	int height = DEFAULT_WINDOW_HEIGHT;
+	if (texture->width > DEFAULT_WINDOW_WIDTH && texture->width + 16 <= monitor_rect.width)
+		width = texture->width;
+	if (texture->height > DEFAULT_WINDOW_HEIGHT && texture->height + 32 <= monitor_rect.height)
+		height = texture->height;
+	gtk_window_set_default_size(GTK_WINDOW(gtk_window), width, height);
 	g_signal_connect(G_OBJECT(gtk_window), "delete_event",
 		G_CALLBACK(delete_event_cb), NULL);
 	g_signal_connect(G_OBJECT(gtk_window), "destroy",
 		G_CALLBACK(destroy_cb), NULL);
 	gtk_container_set_border_width(GTK_CONTAINER(gtk_window), 0);
-	GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-	gtk_container_add(GTK_CONTAINER(gtk_window), vbox);
-	GtkWidget *hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-	gtk_container_add(GTK_CONTAINER(vbox), hbox);
 	GtkWidget *image_drawing_area = gtk_drawing_area_new();
-	gtk_widget_set_size_request(image_drawing_area,
-		DEFAULT_TEXTURE_AREA_WIDTH, DEFAULT_TEXTURE_AREA_HEIGHT);
 	// Create a vbox for texture image and label.
 	GtkWidget *texture_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+	gtk_box_set_homogeneous(GTK_BOX(texture_vbox), FALSE);
 	gtk_box_pack_start(GTK_BOX(texture_vbox), image_drawing_area, TRUE, TRUE, 0);
 	texture_label = gtk_label_new("Unknown");
-	gtk_box_pack_start(GTK_BOX(texture_vbox), texture_label, TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(texture_vbox), texture_label, FALSE, FALSE, 0);
 	// Add the texture vbox to the lay-out.
-	gtk_box_pack_start(GTK_BOX(hbox), texture_vbox, TRUE, FALSE, 8);
-	area_width = DEFAULT_TEXTURE_AREA_WIDTH;
-	area_height = DEFAULT_TEXTURE_AREA_HEIGHT;
-	area_surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32,
-		area_width, area_height);
+	gtk_container_add(GTK_CONTAINER(gtk_window), texture_vbox);
 	g_signal_connect(image_drawing_area, "draw",
-		G_CALLBACK(area_draw_cb), area_surface);
-
+		G_CALLBACK(area_draw_cb), NULL);
+	g_signal_connect(image_drawing_area, "configure-event",
+		G_CALLBACK(area_configure_event_cb), image_drawing_area);
 	gtk_widget_show_all(gtk_window);
 }
 
 static double CalculateZoomFactor() {
-	double factorx = area_width / texture->width;
-	double factory = area_height / texture->height;
+	double factorx = (double)area_width / texture->width;
+	double factory = (double)area_height / texture->height;
 	if (factorx < factory)
 		return factorx;
 	else
@@ -95,13 +115,17 @@ static double CalculateZoomFactor() {
 }
 
 static void DrawTexture(detexTexture *texture, uint8_t *pixel_buffer) {
-	cairo_surface_t *image_surface = cairo_image_surface_create_for_data(
-		(unsigned char *)pixel_buffer, CAIRO_FORMAT_ARGB32,
-		texture->width, texture->height, texture->width * 4);
 	cairo_t *cr = cairo_create(area_surface);
 	cairo_set_operator(cr, CAIRO_OPERATOR_OVER);
 	cairo_set_source_rgba(cr, 0.0d, 0.0d, 0.5d, 1.0d);
 	cairo_paint(cr);
+	if (texture == NULL) {
+		cairo_destroy(cr);
+		return;
+	}
+	cairo_surface_t *image_surface = cairo_image_surface_create_for_data(
+		(unsigned char *)pixel_buffer, CAIRO_FORMAT_ARGB32,
+		texture->width, texture->height, texture->width * 4);
 	cairo_rectangle(cr, 0, 0, area_width, area_height);
 	double zoom_factor = CalculateZoomFactor();
         cairo_scale(cr, zoom_factor, zoom_factor);
@@ -123,9 +147,9 @@ int main(int argc, char **argv) {
 		printf("Syntax: detex-view <texture filename>\n");
 		exit(0);
 	}
-	CreateWindowLayout();
 	const char *filename = argv[1];
 	bool r = LoadCompressedTexture(filename);
+	CreateWindowLayout();
 	if (!r) {
 		printf("%s\n", detexGetErrorMessage());
 		exit(1);
@@ -141,7 +165,7 @@ int main(int argc, char **argv) {
 		pixel_format = DETEX_PIXEL_FORMAT_BGRA8;
 	else
 		pixel_format = DETEX_PIXEL_FORMAT_BGRX8;
-	uint8_t *pixel_buffer = (uint8_t *)malloc(texture->width * texture->height *
+	pixel_buffer = (uint8_t *)malloc(texture->width * texture->height *
 		detexGetPixelSize(pixel_format));
 	r = detexDecompressTextureLinear(texture, pixel_buffer,
 		pixel_format);
